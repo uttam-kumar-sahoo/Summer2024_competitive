12d7e2b06d190234168f3d9808e18b0eb648fb0c
UTTAM KUMAR SAHOO <officialtechwarez@gmail.com>
2024-08-17 19:35:08 +0530

    Update 2037Minimum_Number_of_Moves_to_Seat_Everyone.py

--- 2037Minimum_Number_of_Moves_to_Seat_Everyone.py
+++ 2037Minimum_Number_of_Moves_to_Seat_Everyone.py
@@ -1,3 +1,3 @@
 class Solution:
   def minMovesToSeat(self, seats: List[int], students: List[int]) -> int:
-    return sum(abs(seat - student) for seat, student in zip(sorted(seats), sorted(students)))
+    return sum(abs(seat - student) for seat, student in zip(sorted(seats), sorted(students))) 
